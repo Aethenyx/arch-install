f27434b0d268ddfd183f8e8f2a664dd6328eab33
aethenyx <aethenyx@gmail.com>
2026-04-20 15:18:37 +0530

    install arch

---
+++ arch_install.md
@@ -0,0 +1,131 @@
+# Arch Install
+
+## Connect to Internet
+If you are using wifi, use [iwd](https://wiki.archlinux.org/title/Iwd)
+```
+device list
+station *device* get-networks
+station *d
+```
+
+## Install Guide
+
+1. Turn on parallel downloads
+``sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf``
+
+2. Install archlinux keyring
+``pacman --noconfirm -Sy archlinux-keyring``
+
+3. Load layout, set time
+```
+loadkeys us
+timedatectl set-ntp true
+```
+
+4. Create ext4, efi, swap partition
+```
+lsblk
+cfdisk /dev/sdx or /dev/nvme0x
+
+# Efi partition
+mkfs.fat -F32 $efipartition
+
+# Swap partition
+mkswap $swappartition
+swapon $swappartition
+
+# ext4 partition
+mkfs.ext4 $partition
+```
+
+5. Mount linux partition to /mnt
+``mount $partition /mnt``
+
+6. Pacstrap
+```
+pacstrap /mnt base base-devel linux linux-firmware
+```
+
+7. genstab
+```
+genfstab -U /mnt >> /mnt/etc/fstab
+```
+
+8. chroot
+```
+arch-chroot /mnt
+```
+
+9. set timezone
+```
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+```
+
+10. set hardware clock from system clock
+```
+hwclock --systohc
+```
+
+11. set locale
+```
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "KEYMAP=us" > /etc/vconsole.conf
+```
+
+12. set hostname
+```
+echo $hostname > /etc/hostname
+echo "127.0.0.1       localhost" >> /etc/hosts
+echo "::1             localhost" >> /etc/hosts
+echo "127.0.1.1       $hostname.localdomain $hostname" >> /etc/hosts
+```
+
+13. Initramfs
+```
+mkinitcpio -P
+```
+
+14. Set root password
+```
+passwd
+```
+
+15. install grub
+```
+pacman --noconfirm -S grub efibootmgr os-prober
+mkdir /boot/efi
+mount $efipartition /boot/efi
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=archlinux --recheck
+
+sed -i 's/quiet/pci=noaer/g' /etc/default/grub
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
+
+echo "GRUB_DISABLE_OS_PROBER=false" >>  /etc/default/grub
+
+grub-mkconfig -o /boot/grub/grub.cfg
+```
+
+16. Install all the required user packages
+```
+pacman -S --noconfirm networkmanager git
+```
+
+17. Enable networkmanager
+```
+systemctl enable NetworkManager.service
+```
+
+18. Set user permissions
+```
+echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
+```
+
+19. Create user
+```
+useradd -m -G wheel $username
+passwd $username
+```
+
---
+++ arch_install.sh
@@ -0,0 +1,75 @@
+# == MY ARCH SETUP INSTALLER == #
+#part1
+printf '\033c'
+sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
+pacman --noconfirm -Sy archlinux-keyring
+loadkeys us
+timedatectl set-ntp true
+lsblk
+printf "\e[0;34mEnter the drive: \e[0m"
+read drive
+cfdisk $drive
+printf "\e[0;34mEnter the EFI partition: \e[0m"
+read efipartition
+mkfs.fat -F32 $efipartition
+read -p "Did you also create swap partition? [y/n]" answer
+if [[ $answer = y ]]; then
+  printf "\e[0;34mEnter swap partition: \e[0m"
+  read swappartition
+  mkswap $swappartition
+  swapon $swappartition
+fi
+printf "\e[0;34mEnter the linux filesystem partition: \e[0m"
+read partition
+mkfs.ext4 $partition
+echo "mounting $partition to /mnt"
+mount $partition /mnt
+pacstrap /mnt base base-devel linux linux-firmware
+genfstab -U /mnt >>/mnt/etc/fstab
+sed '1,/^#part2/d' arch_install.sh >/mnt/arch_install2.sh
+chmod +x /mnt/arch_install2.sh
+arch-chroot /mnt ./arch_install2.sh
+exit
+
+#part2
+printf '\033c'
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" >>/etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >/etc/locale.conf
+echo "KEYMAP=us" >/etc/vconsole.conf
+printf "\e[0;34mHostname: \e[0m"
+read hostname
+echo $hostname >/etc/hostname
+echo "127.0.0.1       localhost" >>/etc/hosts
+echo "::1             localhost" >>/etc/hosts
+echo "127.0.1.1       $hostname.localdomain $hostname" >>/etc/hosts
+mkinitcpio -P
+passwd
+pacman --noconfirm -S grub efibootmgr os-prober
+printf "\e[0;34mEnter EFI partition: \e[0m"
+read efipartition
+mkdir /boot/efi
+mount $efipartition /boot/efi
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=archlinux --recheck
+read -rp "Do you want to set GRUB_TIMEOUT=0? (y/N): " confirm
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+  sed -i 's/quiet/pci=noaer/g' /etc/default/grub
+  sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
+fi
+read -rp "Do you want to enable probing for other operating systems? (y/N): " confirm
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+  echo "GRUB_DISABLE_OS_PROBER=false" >>/etc/default/grub
+fi
+grub-mkconfig -o /boot/grub/grub.cfg
+
+pacman -S --noconfirm networkmanager git
+
+systemctl enable NetworkManager.service
+echo "%wheel ALL=(ALL:ALL) ALL" >>/etc/sudoers
+printf "\e[0;34mEnter Username: \e[0m"
+read username
+useradd -m -G wheel $username
+passwd $username
+echo "Installation Finish Reboot now"
